88c6dd4949aad3790176e7efd96cf872db4470bb
Matias Bontempo <matias.bontempo@gmail.com>
2020-03-18 23:12:06 -0300

    feat: can't believe i've never commited

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+  "name": "SuperFight!",
+  "icons": [
+    {
+      "src": "./img/superfight.png",
+      "sizes": "48x48",
+      "type": "image/png",
+      "density": "1.0"
+    },
+    {
+      "src": "./img/superfight-hd.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "density": "1.0"
+    }
+  ],
+  "start_url": "index.html",
+  "display": "standalone",
+  "orientation": "portrait"
+}
